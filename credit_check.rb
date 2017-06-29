1994e03d668515758f59616dfb984bf18faf2313
Mike Heft <mikeheft@gmail.com>
2017-06-29 07:45:05 -0600

    added file i/o

--- credit_check.rb
+++ credit_check.rb
@@ -1,7 +1,12 @@
 require './lib/credit_check_validate'
 
-puts "Enter credit card number to validate: "
-print "> "
-cc_num = gets.chomp
-new_num = CreditCheck.new(cc_num)
-puts new_num.validate
+# puts "Enter credit card number to validate: "
+# print "> "
+# cc_num = gets.chomp
+# new_num = CreditCheck.new(cc_num)
+# puts new_num.validate
+filename = "./card_numbers.txt"
+File.foreach filename do |number|
+  card = CreditCheck.new(number.chomp)
+  puts "The number #{number.chomp} is #{card.validate}"
+end
